561a73b57fa518abe6647af87ac5361c1b6a1abb
Moises Navarro <moisalejandro@gmail.com>
2024-05-04 18:35:38 -0600

    fix

--- cfdi_import/wizards/cfdi_importer.py
+++ cfdi_import/wizards/cfdi_importer.py
@@ -191,7 +191,7 @@ class CFDIImporter(models.TransientModel):
         for concepto in cfdi["Conceptos"]["Concepto"]:
             taxes = self.get_taxes(cfdi, concepto)
             discount_amount = float(concepto.get("@Descuento", 0))
-            discount_percent = discount_amount / float(concepto["@Importe"])
+            discount_percent = discount_amount / float(concepto["@Importe"]) * 100
             lines.append(
                 (
                     0,
