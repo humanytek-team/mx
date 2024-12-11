d92cb104c8a686f11ffb713e43a9b545534b6ada
Moises Navarro <moisalejandro@gmail.com>
2024-12-11 15:31:34 -0600

    feat(factoraje): fix new xml

--- l10n_mx_factoraje/models/account_move.py
+++ l10n_mx_factoraje/models/account_move.py
@@ -7,3 +7,8 @@ class AccountMove(models.Model):
     thirdparty_partner_id = fields.Many2one(
         related="payment_id.thirdparty_partner_id",
     )
+
+    def _l10n_mx_edi_add_payment_cfdi_values(self, cfdi_values, pay_results):
+        res = super()._l10n_mx_edi_add_payment_cfdi_values(cfdi_values, pay_results)
+        cfdi_values["thirdparty_partner_id"] = self.thirdparty_partner_id
+        return res
