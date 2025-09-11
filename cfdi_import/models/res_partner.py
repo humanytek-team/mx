913d76b41ee33729e1caf93200cffa29ad654922
Rene Villegas <rene.villegas@wizeline.com>
2025-09-10 19:04:13 -0600

    [FIX]res.partner model

--- cfdi_import/models/res_partner.py
+++ cfdi_import/models/res_partner.py
@@ -1,7 +1,7 @@
 from odoo import _, api, fields, models
 
 
-class ResPartner(models.model):
+class ResPartner(models.Model):
     _inherit = "res.partner"
 
     ignore_import_xml = fields.Bool(string="Ignore partner in XML Importer")
