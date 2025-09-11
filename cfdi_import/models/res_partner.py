500832affffb302d3a611e8b34ede53ac2361c8e
Rene Villegas <rene.villegas@wizeline.com>
2025-09-10 19:07:48 -0600

    [FIX]new field type to Boolean

--- cfdi_import/models/res_partner.py
+++ cfdi_import/models/res_partner.py
@@ -4,4 +4,4 @@ from odoo import _, api, fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    ignore_import_xml = fields.Bool(string="Ignore partner in XML Importer")
+    ignore_import_xml = fields.Boolean(string="Ignore partner in XML Importer")
