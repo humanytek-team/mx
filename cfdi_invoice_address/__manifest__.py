ee2bccbc304d8029c232dac49264cf07c5069eaa
Moises Navarro <moisalejandro@gmail.com>
2025-09-20 15:35:31 -0600

    feat: cfdi_invoice_address

---
+++ cfdi_invoice_address/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "CFDI Invoice Address",
+    "version": "1.0",
+    "author": "Humanytek",
+    "website": "https://humanytek.com",
+    "depends": [
+        "l10n_mx_edi",
+    ],
+    "data": [
+        # security
+        # data
+        # reports
+        # views
+        # wizards
+    ],
+    "license": "LGPL-3",
+}
